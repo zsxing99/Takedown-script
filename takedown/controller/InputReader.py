799d465e7c6bb5de82c694a0b2bde60bdbd8fe83
Zesheng Xing <44153625+zsxing99@users.noreply.github.com>
2020-11-04 21:14:55 -0800

    -set up input reader structure

--- takedown/controller/InputReader.py
+++ takedown/controller/InputReader.py
@@ -11,12 +11,46 @@ class InputReader:
 
     def __init__(self):
         self.raw_input = sys.argv
+        self.parse_error_msg = None
         self.command_type = None
         self.required_inputs = {}
         self.optional_inputs = {}
 
-    def command_find(self):
+    def prepare(self):
         """
-        Command validator for "takedown find"
+        check semantics
+        :return: true if commands are correct; false if failed
+        """
+        if self.raw_input[1] == "find":
+            return self.__command_find()
+        elif self.raw_input[1] == "send":
+            return self.__command_send()
+        else:
+            return self.__command_help()
+
+    def __command_find(self):
+        """
+        Command validator and parser for "takedown find"
         :return:
+        """
+        pass
+
+    def __command_send(self):
+        """
+        Command validator and parser for "takedown send"
+        :return:
+        """
+        pass
+
+    def __command_help(self):
+        """
+        Command parser for help
+        :return:
+        """
+        pass
+
+    def execute(self):
+        """
+        it will be executed only no error in prepare
+        :return: return a dict as the { param : key }
         """
