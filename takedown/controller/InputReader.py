4b8bcdf74c2762a5b6423e6542d6cfa89820bce7
Zesheng Xing <44153625+zsxing99@users.noreply.github.com>
2020-11-08 19:21:52 -0800

    -implemented "find" command -added test suits

--- takedown/controller/InputReader.py
+++ takedown/controller/InputReader.py
@@ -7,10 +7,28 @@ Input Reader that checks and distributes commands. For detailed list of command 
 import sys
 
 
+def check_file(file_path, mode="r"):
+    """
+    check if provided file path is able to be accessed
+    :return: true if can, false otherwise
+    """
+    f = None
+    try:
+        f = open(file_path, mode)
+        f.close()
+        return True
+    except IOError:
+        if f:
+            f.close()
+        return False
+
+
 class InputReader:
 
-    def __init__(self):
-        self.raw_input = sys.argv
+    def __init__(self, command_input=None):
+        if command_input is None:
+            command_input = sys.argv
+        self.raw_input = command_input
         self.parse_error_msg = None
         self.command_type = None
         self.required_inputs = {}
@@ -33,14 +51,70 @@ class InputReader:
         Command validator and parser for "takedown find"
         :return:
         """
-        self.command_type = ""
-        pass
+        self.command_type = "find"
+
+        # read required input: search_query and GitHub token
+        if len(self.raw_input) < 4:
+            self.parse_error_msg = "Missing required parameters. Please refer to 'help' command"
+            return False
+        else:
+            self.required_inputs["search_query"] = self.raw_input[2]
+            self.required_inputs["GitHub_token"] = self.raw_input[3]
+            print("checked required parameters")
+
+        # keep reading optional parameters
+        length = len(self.raw_input)
+        curr = 4
+        while curr < length:
+            # -t target
+            if self.raw_input[curr] == '-t':
+                if curr == length - 1:
+                    self.parse_error_msg = "Missing target after flag '-t'"
+                    return False
+                else:
+                    targets = self.raw_input[curr + 1].split("+")
+                    for target in targets:
+                        if target not in ["file", "code"]:
+                            self.parse_error_msg = "Unrecognized target, check 'help' for details."
+                            return False
+                    self.optional_inputs["target"] = targets
+            elif self.raw_input[curr] == '-i':
+                if curr == length - 1:
+                    self.parse_error_msg = "Missing target after flag '-i'"
+                    return False
+                else:
+                    files = self.raw_input[curr + 1].split("+")
+                    for file in files:
+                        if not check_file(file):
+                            self.parse_error_msg = "File path '{}' cannot be accessed.".format(file)
+                            return False
+                    self.optional_inputs["inputs"] = files
+                    self.command_type = "find+compare"
+            elif self.raw_input[curr] == '-o':
+                if curr == length - 1:
+                    self.parse_error_msg = "Missing target after flag '-o'"
+                    return False
+                else:
+                    file = self.raw_input[curr + 1]
+                    if not check_file(file, "w+"):
+                        self.parse_error_msg = "Output file path '{}' cannot be accessed.".format(file)
+                        return False
+                    self.optional_inputs["output"] = file
+            else:
+                # skip unrecognized input
+                curr += 1
+                continue
+            curr += 2
+
+        print("checked optional parameters")
+        return True
 
     def __command_send(self):
         """
         Command validator and parser for "takedown send"
         :return:
         """
+        self.command_type = "send"
         pass
 
     def __command_help(self):
@@ -55,4 +129,6 @@ class InputReader:
         it will be executed only no error in prepare
         :return: return a dict as the { param : key }
         """
-        pass
+        if self.parse_error_msg:
+            return self.parse_error_msg
+        return self.required_inputs, self.optional_inputs
